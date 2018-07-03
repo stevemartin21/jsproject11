4079f59791c1b447fc143a5a302d0e27c85f3c43
Steve Martin <stevemartinrealestate@gmail.com>
2018-07-03 13:03:56 -0600

    Add files via upload

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -65,7 +65,7 @@ router.get('/:courseId', function(req,res, next){
 //Creat new course// Almost done
 
 router.post('/', mid.check, function(req, res, next){
-	Course.create(req.body,  function(error){
+	Course.create(req.body,  function(error, course){
 		console.log('so far so good')
 		if(error){
 			// Set error
@@ -74,9 +74,9 @@ router.post('/', mid.check, function(req, res, next){
 			next(err)
 		}else{
 			// set status and location
-			console.log('A Course was created')
+			res.json(course)
 			res.status(201);
-			res.location('/')
+			//res.location('/')
 		}
 	})
 
@@ -88,14 +88,17 @@ router.post('/', mid.check, function(req, res, next){
 
 router.put('/:courseId', [mid.check], function(req, res, next){
 	console.log(req.params.courseId)
-	Course.findByIdAndUpdate(req.params.courseId, req.body, function(error){
+	Course.findByIdAndUpdate(req.params.courseId, { $set: req.body }, function(error, course){
+		console.log('You got it')
 		if(error){
 			var err = new Error('There was an error, check it again')
 			err.status = 400
 			next(err)
 		}
+			console.log('its been updated')
+			//res.json(course)
 			res.status = 200;
-			res.location('/')
+			res.location('/:courseid')
 		
 	})
 })
@@ -104,19 +107,20 @@ router.put('/:courseId', [mid.check], function(req, res, next){
 // POST /api/courses/:courseId/reviews 201 - Creates a review for the specified course ID, sets the Location header to the related course, and returns no content
 // create a new review 
 // Must authenticate
+// create a review and add it to the review array on the course 
 router.post('/:courseId/reviews',[mid.check], function(req, res, next){
 
 	Review.create(req.body,  function(error, review){
 		if(error){
-			var err = new Error('There was an error')
+			var err = new Error('There was an error in creating the review')
 			err.status = 400;
 			next(err)
 		}else{
-			Course.findById(req.params.courseId, { $push: { reviews: review_Id } }, function(error){
+			Course.findById(req.params.courseId, { $push: { reviews: review._Id } }, function(error, course){
 				if(error){
 					// Set Errors
-					var err = new Error('There was an error')
-					err.status(400)
+					var err = new Error('There was an error adding to array')
+					err.status =400
 					next(err)
 				}else{
 					//set back status and return to lcoation
@@ -129,50 +133,11 @@ router.post('/:courseId/reviews',[mid.check], function(req, res, next){
 		}
 	})
 
-	// need to add new review to courseId araay
+	
 })
-
-
-
 
 
 
 module.exports = router;
 
 
-/*
-
-Use this middleware in the following routes:
-POST /api/courses
-PUT /api/courses/:courseId
-GET /api/users
-POST /api/courses/:courseId/reviews
-
-
-Update any POST and PUT routes to return Mongoose validation errors.
-Use the next function in each route to pass any Mongoose validation errors to Express’s global error handler
-Send the Mongoose validation error with a400 status code to the user
-
-
-Set up the following routes (listed in the format HTTP VERB Route HTTP Status Code):
-GET /api/courses 200 - Returns the Course "_id" and "title" properties
-GET /api/course/:courseId 200 - Returns all Course properties and related documents for the provided course ID
-When returning a single course for the GET /api/courses/:courseId route, use Mongoose population to load the related user and reviews documents.
-POST /api/courses 201 - Creates a course, sets the Location header, and returns no content
-PUT /api/courses/:courseId 204 - Updates a course and returns no content
-POST /api/courses/:courseId/reviews 201 - Creates a review for the specified course ID, sets the Location header to the related course, and returns no content
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
